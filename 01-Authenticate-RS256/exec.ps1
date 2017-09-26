f1fb9cec9ef969f3b047fe0b2cafaea0ec4c89fe
Alexis Luque <aluque@indexlabs.com.uy>
2017-09-26 18:26:53 -0300

    Update Dockerfile

---
+++ 01-Authenticate-RS256/exec.ps1
@@ -0,0 +1,2 @@
+docker build -t auth0-php-api-01-authenticate .
+docker run --env-file .env -p 3001:3001 -it auth0-php-api-01-authenticate
---
+++ 02-Authenticate-HS256/exec.ps1
@@ -0,0 +1,2 @@
+docker build -t auth0-php-api-02-authenticate .
+docker run --env-file .env -p 3001:3001 -it auth0-php-api-02-authenticate
